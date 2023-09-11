966de5308998c9d897a12bdc2a49109cd609dbed
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-11 20:53:48 -0300

    feat(aula03): ex03 student grande avarage calculator

---
+++ aula03/ex03.py
@@ -0,0 +1,14 @@
+from digitInput import floatInput
+
+student_grades = [floatInput(f'Insira sua {n}ª nota:\n>>>', lambda value: 0 <= value <= 10) for n in range(3)]
+
+grade_avarage = sum(student_grades)/len(student_grades)
+
+print(f(f'Sua média é {grade_avarage}'))
+
+if grade_avarage<3:
+    print('reprovado')
+elif grade_avarage<7:
+    print('exame de recuperação')
+else:
+    print('aprovado')
