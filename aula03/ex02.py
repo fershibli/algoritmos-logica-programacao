d07405d0eedab267bb1470a35358a81c72da8277
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-11 20:27:54 -0300

    feat(aula03): ex02 picks the lowest value

---
+++ aula03/ex02.py
@@ -0,0 +1,9 @@
+def intInput(input_text):
+    while not (value := input(input_text)).isdigit():
+        print(f'O valor \"{value}\" não é um número. Por favor insira um número.')
+    return int(value)
+
+
+a,b = [intInput(f'Digite o {n+1}º número:\n>>>') for n in range(2)]
+
+print(f'O menor número é {a if a < b else b}')
