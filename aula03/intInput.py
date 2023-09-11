e794e48ba5719fb284bd369cb632a233a118ae04
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-11 20:42:50 -0300

    feat(Aula03.intInput): args for extra conditions

--- aula03/intInput.py
+++ aula03/intInput.py
@@ -1,4 +1,4 @@
-def intInput(input_text):
-    while not (value := input(input_text)).isdigit():
+def intInput(input_text, condition=lambda value: True):
+    while not (value := input(input_text)).isdigit() and condition(value):
         print(f'O valor \"{value}\" não é um número. Por favor insira um número.')
     return int(value)
