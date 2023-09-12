9e879573d674ff13f1766aa5f976bce43791c6c1
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-11 21:26:09 -0300

    fix(Aula03.digitInput): intInput and floatInput return

--- aula03/digitInput.py
+++ aula03/digitInput.py
@@ -1,15 +1,11 @@
-def digitInput(input_text, condition=lambda value: True, parse_float=False):
-    while not (value := input(input_text)).isdigit() and condition(value):
+def digitInput(input_text, condition=lambda value: True):
+    while (value := input(input_text)) and not value.replace('.','',1).isdigit() and condition(value):
         print(f'O valor \"{value}\" não é um número. Por favor insira um número.')
-    if parse_float:
-        return float(value)
-    else:
-        return int(value)
-
+    return value
 
 def intInput(input_text, condition=lambda value: True):
-    return digitInput(input_text, condition, False)
+    return int(floatInput(input_text, condition))
 
 
 def floatInput(input_text, condition=lambda value: True):
-    return digitInput(input_text, condition, False)
+    return float(digitInput(input_text, condition))
