f016a40a3b3b92fbc16ab03707a440d0ccc40978
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-11 21:27:01 -0300

    fix(aula03.ex03): print f-string syntax error

--- aula03/ex03.py
+++ aula03/ex03.py
@@ -1,10 +1,10 @@
 from digitInput import floatInput
 
-student_grades = [floatInput(f'Insira sua {n}ª nota:\n>>>', lambda value: 0 <= value <= 10) for n in range(3)]
+student_grades = [floatInput(f'Insira sua {n+1}ª nota:\n>>>', lambda value: 0 <= float(value) <= 10) for n in range(3)]
 
 grade_average = sum(student_grades) / len(student_grades)
 
-print(f(f'Sua média é {grade_average:5.2f}'))
+print(f'Sua média é {grade_average:5.2f}')
 
 if grade_average<3:
     print('reprovado')
