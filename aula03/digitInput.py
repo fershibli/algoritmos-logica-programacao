9d12c822519a5916313959607c3e4650793a3ea0
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-11 21:39:40 -0300

    fix(Aula03.digitInput): conditional operators

--- aula03/digitInput.py
+++ aula03/digitInput.py
@@ -1,7 +1,10 @@
 def digitInput(input_text, condition=lambda value: True):
-    while (value := input(input_text)) and not value.replace('.','',1).isdigit() and condition(value):
+    while (value := input(input_text)) and \
+            not value.replace('.', '', 1).isdigit() or \
+            not condition(float(value)):
         print(f'O valor \"{value}\" não é um número. Por favor insira um número.')
     return value
+
 
 def intInput(input_text, condition=lambda value: True):
     return int(floatInput(input_text, condition))
