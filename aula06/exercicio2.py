934b673849fd0208795f4f93b51967062ec4c2f9
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-18 20:23:27 -0300

    fix: exercicio2 lesser-or-equal than value

--- aula06/exercicio2.py
+++ aula06/exercicio2.py
@@ -1,9 +1,9 @@
 purchase_value = float(input('Digite o valor da compra em forma de número FLOAT:\n>>>'))
 print(f'Sua compra no valor de {purchase_value} receberá o desconto de ', end='')
-if purchase_value < 1000:
+if purchase_value <= 1000:
     print('10%.')
     purchase_value*=0.9
-elif purchase_value <5000:
+elif purchase_value <= 5000:
     print('20%.')
     purchase_value*=0.8
 else:
