10f782fda3feefe0136b26764e236508fddceb78
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-18 19:36:04 -0300

    fix(aula06): string new line

--- aula06/exercicio1.py
+++ aula06/exercicio1.py
@@ -1,2 +1,2 @@
-number = int(input('Digite um número INTEIRO:\>>>'))
+number = int(input('Digite um número INTEIRO:\n>>>'))
 print(f'O número {number} {"é" if number%3==0 and number%5==0 else "não é"} divisível por 3 e por 5 ao mesmo tempo.')
