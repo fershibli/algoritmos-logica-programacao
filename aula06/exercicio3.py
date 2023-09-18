a6bee8708c51998f905433fc211322f87cdade9b
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-18 20:24:17 -0300

    fix: exercicio3 remaining liters calculation

--- aula06/exercicio3.py
+++ aula06/exercicio3.py
@@ -12,15 +12,18 @@ length_area = walls_length*walls_height
 
 # dobro de paredes (cômodo quadrado), menos porta
 total_area = ((width_area+length_area)*2)-door_area
-print(f'A área total a ser pintada é de {total_area}m².')
+print(f'A área total a ser pintada é de {total_area:.2f}m².')
 
 # a cada 3 metros quadrados = 1 L, basta dividir todos os metros quadrados por 3
 tint_liters = total_area/3
-print(f'Para pintar essa área são necessários {tint_liters}L de tinta.')
+print(f'Para pintar essa área são necessários {tint_liters:.2f}L de tinta.')
 
 # a cada lata = 18L 
 tin_quantity = floor(tint_liters/18)
 # o que sobrar da divisão da lata, dividir por 3.6
 gallon_quantity = floor((tint_liters%18)/3.6)
 
-print(f'Você precisará de:\n-> {tin_quantity} latas de tinta\n-> {gallon_quantity} galões de tinta\n-> Faltarão {tint_liters-gallon_quantity-tin_quantity}L para completar.')
+print('Você precisará ter:')
+print(f'> {tin_quantity} latas de tinta;')
+print(f'> {gallon_quantity} galões de tinta;')
+print(f'> e ainda faltarão {tint_liters-(tin_quantity*18)-(gallon_quantity*3.6):.2f}L de tinta para completar a pintura.')
