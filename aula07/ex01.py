bc7ea0e4b5b0acfe3212f01cc86922f84209c00d
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-25 19:59:19 -0300

    feat(aula07): ex01 usuario decide tamanho do loop

--- aula07/ex01.py
+++ aula07/ex01.py
@@ -1,6 +1,12 @@
+leituras = int(input("Quantas idades você quer que eu leia?\n>>>"))
+if leituras < 1:
+    raise Exception("O número de leituras não pode ser negativo.")
+
 soma = 0
-for i in range(1, 11):
+for i in range(1, leituras+1):
     n = int(input(f"Entre com o {i}a. idade:\n>>>"))
     soma += n
+
 media = soma / i
+
 print(f"A média das {i} idades é {media:5.2f}")
