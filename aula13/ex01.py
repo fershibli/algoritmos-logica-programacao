33d7f216fb2f4a337f68e150cc9683c83c81f481
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-11-06 20:38:37 -0300

    feat(aula13): isEven function

---
+++ aula13/ex01.py
@@ -0,0 +1,11 @@
+def isEven(number):
+    return number % 2 == 0
+
+
+print(isEven(0))
+print(isEven(1))
+print(isEven(2))
+print(isEven(11))
+print(isEven(12))
+print(isEven(-1))
+print(isEven(-2))
